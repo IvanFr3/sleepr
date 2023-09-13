c8a644308f35fca801c57bb21166d1bf1a7a3275
FilipovIvan <ivanfilipov45@yahoo.bg>
2023-09-13 17:24:00 +0300

    Add common auth guard

--- apps/reservations/src/main.ts
+++ apps/reservations/src/main.ts
@@ -3,16 +3,18 @@ import { ValidationPipe } from '@nestjs/common';
 import { Logger } from 'nestjs-pino';
 import { ReservationsModule } from './reservations.module';
 import { ConfigService } from '@nestjs/config';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(ReservationsModule);
+  const configService = app.get(ConfigService);
+  app.use(cookieParser());
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
     }),
   );
   app.useLogger(app.get(Logger));
-  const configService = app.get(ConfigService);
   await app.listen(configService.get('PORT'));
 }
 bootstrap();
