2e87cd16e601f92894875546856a5c74d38286f1
FilipovIvan <ivanfilipov45@yahoo.bg>
2023-09-14 18:24:40 +0300

    Fix stripe not accepting real card numbers

--- apps/payments/src/payments.service.ts
+++ apps/payments/src/payments.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { CreateChargeDto } from '../dto/create-charge.dto';
+import { CreateChargeDto } from '@app/common';
 import { ConfigService } from '@nestjs/config';
 import Stripe from 'stripe';
 
@@ -13,17 +13,17 @@ export class PaymentsService {
   constructor(private readonly configService: ConfigService) {}
 
   async createCharge({ card, amount }: CreateChargeDto) {
-    const paymentMethod = await this.stripe.paymentMethods.create({
-      type: 'card',
-      card,
-    });
+    // const paymentMethod = await this.stripe.paymentMethods.create({
+    //   type: 'card',
+    //   card,
+    // });
 
     const paymentIntent = await this.stripe.paymentIntents.create({
-      payment_method: paymentMethod.id,
+      payment_method: 'pm_card_visa',
       amount: amount * 100,
-      currency: 'usd',
-      payment_method_types: ['card'],
       confirm: true,
+      payment_method_types: ['card'],
+      currency: 'usd',
     });
 
     return paymentIntent;
