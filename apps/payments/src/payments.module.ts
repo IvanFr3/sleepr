6221f9ad82627d46af44e47459cf6b2aa7b01c93
FilipovIvan <ivanfilipov45@yahoo.bg>
2023-09-14 16:20:09 +0300

    Integrate stripe api for charging cards

--- apps/payments/src/payments.module.ts
+++ apps/payments/src/payments.module.ts
@@ -1,9 +1,21 @@
+import * as Joi from 'joi';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { LoggerModule } from '@app/common';
 import { PaymentsController } from './payments.controller';
 import { PaymentsService } from './payments.service';
 
 @Module({
-  imports: [],
+  imports: [
+    LoggerModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        PORT: Joi.number().required(),
+        STRIPE_SECRET_KEY: Joi.string().required(),
+      }),
+    }),
+  ],
   controllers: [PaymentsController],
   providers: [PaymentsService],
 })
--- apps/payments/src/payments.service.ts
+++ apps/payments/src/payments.service.ts
@@ -1,8 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { CreateChargeDto } from '../dto/create-charge.dto';
+import { ConfigService } from '@nestjs/config';
+import Stripe from 'stripe';
 
 @Injectable()
 export class PaymentsService {
-  getHello(): string {
-    return 'Hello World!';
+  private readonly stripe = new Stripe(
+    this.configService.get('STRIPE_SECRET_KEY'),
+    { apiVersion: '2023-08-16' },
+  );
+
+  constructor(private readonly configService: ConfigService) {}
+
+  async createCharge({ card, amount }: CreateChargeDto) {
+    const paymentMethod = await this.stripe.paymentMethods.create({
+      type: 'card',
+      card,
+    });
+
+    const paymentIntent = await this.stripe.paymentIntents.create({
+      payment_method: paymentMethod.id,
+      amount: amount * 100,
+      currency: 'usd',
+      payment_method_types: ['card'],
+      confirm: true,
+    });
+
+    return paymentIntent;
   }
 }
